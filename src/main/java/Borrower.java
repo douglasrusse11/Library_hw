68c9483ad5f27bc2581611b5a2d067a94733ebf1
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-11 16:46:15 +0000

    passes tests for Borrower

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private String name;
+    private ArrayList<Book> books;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.books = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNumberOfBooks() {
+        return this.books.size();
+    }
+
+    public void borrowBook(Book book, Library library) {
+        if (library.removeBook(book)) {
+            this.books.add(book);
+        }
+    }
 }
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -29,4 +29,7 @@ public class Library {
     }
 
 
+    public boolean removeBook(Book book) {
+        return this.books.remove(book);
+    }
 }
